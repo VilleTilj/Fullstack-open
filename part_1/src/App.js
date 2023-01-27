7e5cc663c9fb0e51d046eac2b0053749e214825c
Ville Tiljander <ville.tiljander123@gmail.com>
2023-01-27 16:19:33 +0200

    Compeleted first task, react components in App

--- part_1/src/App.js
+++ part_1/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const Header = (props) => {
+  console.log(props) 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>{props.course}</h1>
+  )
 }
 
-export default App;
+const Content = (props) => {
+  console.log(props) 
+  return (
+    <p>
+      {props.part} {props.ex}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  console.log(props) 
+  return (
+    <p>
+      Number of exercises {props.count}
+    </p>
+  )
+  
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} ex={exercises1} />
+      <Content part={part2} ex={exercises2} />
+      <Content part={part3} ex={exercises3} />
+      <Total count={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+export default App
