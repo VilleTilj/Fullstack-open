415ed8390c2459f86572cc044e2479ffdecc7a19
Ville Tiljander <ville.tiljander123@gmail.com>
2023-02-18 12:03:45 +0200

    Exercise 2.1 skeleton

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,14 +1,56 @@
-const App = (props) => {
-  const { notes } = props
+
+const Header = (props) => {
+  console.log(props) 
+  return (
+    <h1>{props.course.name}</h1>
+  )
+}
+
+const Content = (props) => {
+  console.log(props) 
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  console.log(props) 
+  return (
+    <p>
+      Number of exercises {props.sum[0].exercises + props.sum[1].exercises + props.sum[2].exercises}
+    </p>
+  )
+  
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        <li>{notes[0].content}</li>
-        <li>{notes[1].content}</li>
-        <li>{notes[2].content}</li>
-      </ul>
+      <Header course={course} />
+      <Content part={course.parts[0]} />
+      <Content part={course.parts[1]} />
+      <Content part={course.parts[2]} />
+      <Total sum={course.parts} />
     </div>
   )
 }
